44b8e5fa67eb1582bafd72e5f2354175264be0de
Richard Nienaber <rjnienaber@gmail.com>
2012-05-05 17:34:04 +0100

    Added extra logging to mvc test app

--- WebFiles.Web.Tests/Controllers/WebDavController.cs
+++ WebFiles.Web.Tests/Controllers/WebDavController.cs
@@ -5,15 +5,35 @@ using System.Web;
 using System.Web.Mvc;
 using WebFiles.Mvc;
 using WebFiles.Mvc.Providers;
+using NLog;
+using System.Diagnostics;
 
 namespace WebFiles.Web.Tests.Controllers
 {
     public class WebDavController : WebFilesController 
     {
+        private static Logger logger = LogManager.GetCurrentClassLogger();
+
         public WebDavController()
         {
             this.config = new Configuration { RootPath = "D:\\stuff" };
             this.storageProvider = new FileSystemProvider();
         }
+
+        protected override void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            HttpContext.Items["Time_Action"] = Stopwatch.StartNew();
+            base.OnActionExecuting(filterContext);
+        }
+
+        protected override void OnActionExecuted(ActionExecutedContext filterContext)
+        {
+            base.OnActionExecuted(filterContext);
+            var stopwatch = HttpContext.Items["Time_Action"] as Stopwatch;
+            stopwatch.Stop();
+            var pathInfo = filterContext.RouteData.Values["pathInfo"];
+            logger.Debug("Method: {0}, PathInfo: {1}, Time: {2}(ms)", Request.HttpMethod, pathInfo, stopwatch.ElapsedMilliseconds);
+        }
+
     }
 }
